ff59f5238e02d608f42ab20216dfccdb408d01a0
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2015-06-11 11:27:01 +0200

    [Integration] Crossover integrated + requirements.

--- crossover.py
+++ crossover.py
@@ -3,113 +3,113 @@ import copy
 import random
 
 class Appointment():
-    def __init__(self, idAppointment, timestamp):
-        self.idAppointment = idAppointment;
-        self.timestamp = timestamp;
+    def __init__(self, idAppointment, starting_time):
+        self.idAppointment = idAppointment
+        self.starting_time = starting_time
 
     def __repr__(self):
-        return "<%d, %d>" % self._asPrintable();
+        return "<%d, %d>" % self._asPrintable()
 
     def _asPrintable(self):
-        return (self.idAppointment, self.timestamp);
+        return (self.idAppointment, self.starting_time)
 
     def _idAppointment(self):
-        return self.idAppointment;
+        return self.idAppointment
 
-    def _timestamp(self):
-        return self.timestamp;
+    def _starting_time(self):
+        return self.starting_time
 
 
 
 class Vehicle():
     def __init__(self, idVehicle, nbAppointment):
-        self.idVehicle = idVehicle;
-        self.nbAppointment = nbAppointment;
+        self.idVehicle = idVehicle
+        self.nbAppointment = nbAppointment
 
     def _idVehicle(self):
-        return self.idVehicle;
+        return self.idVehicle
 
     def _nbAppointment(self):
-        return self.nbAppointment;
+        return self.nbAppointment
 
     def _asPrintable(self):
-        return (self.idVehicle, self.nbAppointment);
+        return (self.idVehicle, self.nbAppointment)
 
     def __repr__(self):
-        return "<%d %d>" % self._asPrintable();
+        return "<%d %d>" % self._asPrintable()
 
 
 
-class Individual():
-    def __init__(self, appointments, vehicles):
-        sumVehicles = 0;
+class MvrpIndividual():
+    def __init__(self, routes, vehicles):
+        sumVehicles = 0
         for i in range(0, len(vehicles)):
-            sumVehicles += vehicles[i]._nbAppointment();
+            sumVehicles += vehicles[i]._nbAppointment()
 
-        if (len(appointments) == sumVehicles):
-            self.appointments = list(appointments);
-            self.vehicles = list(vehicles);
+        if (len(routes) == sumVehicles):
+            self.routes = list(routes)
+            self.vehicles = list(vehicles)
 
-    def _appointments(self):
-        return self.appointments;
+    def _routes(self):
+        return self.routes
 
     def _vehicles(self):
-        return self.vehicles;
+        return self.vehicles
 
 
 
 def insertAppointment(app, appointment2D):
 
-    tmp = len(appointment2D);
-    no_vehicle = random.randrange(0, tmp);
+    tmp = len(appointment2D)
+    no_vehicle = random.randrange(0, tmp)
     for idx in range(0, len(appointment2D[no_vehicle])):
-        if (appointment2D[no_vehicle][idx]._timestamp() > app._timestamp()):
-            appointment2D[no_vehicle].insert(idx, copy.deepcopy(app));
-            return appointment2D;
+        if (appointment2D[no_vehicle][idx]._starting_time() > app._starting_time()):
+            appointment2D[no_vehicle].insert(idx, copy.deepcopy(app))
+            return appointment2D
 
-    appointment2D[no_vehicle].append(app);
-    return appointment2D;
+    appointment2D[no_vehicle].append(app)
+    return appointment2D
 
 def crossOver(parent1, parent2):
 
-    random.seed(666);
+    random.seed(666)
 
     # Can't compute offspring in this case
-    if (len(parent1._appointments()) != len(parent2._appointments())):
-        return copy.deepcopy(parent1);
+    if (len(parent1._routes()) != len(parent2._routes())):
+        return copy.deepcopy(parent1)
 
-    appointmentsByVehicle1 = [];
-    appointmentsByVehicle2 = [];
+    appointmentsByVehicle1 = []
+    appointmentsByVehicle2 = []
 
-    tmpLen = len(parent1.vehicles);
+    tmpLen = len(parent1.vehicles)
 
-    offset1 = 0;
-    offset2 = 0;
+    offset1 = 0
+    offset2 = 0
 
     # Getting lists of lists containing all the appointments sorted by vehicle
     for i in range(0, tmpLen):
 
-        newOffset1 = offset1 + parent1._vehicles()[i]._nbAppointment();
-        newOffset2 = offset2 + parent2._vehicles()[i]._nbAppointment();
+        newOffset1 = offset1 + parent1._vehicles()[i]._nbAppointment()
+        newOffset2 = offset2 + parent2._vehicles()[i]._nbAppointment()
 
         appointmentsByVehicle1.append(
-                copy.deepcopy(parent1._appointments()[offset1:newOffset1])
-                );
+                copy.deepcopy(parent1._routes()[offset1:newOffset1])
+                )
 
         appointmentsByVehicle2.append(
-                parent2._appointments()[offset2:newOffset2]
-                );
+                parent2._routes()[offset2:newOffset2]
+                )
 
-        offset1 = newOffset1;
-        offset2 = newOffset2;
+        offset1 = newOffset1
+        offset2 = newOffset2
 
     print("Before: ")
-    print(appointmentsByVehicle1);
-    print(appointmentsByVehicle2);
+    print(appointmentsByVehicle1)
+    print(appointmentsByVehicle2)
 
     # Picking and removing appointments associated to a vehicle in the second
     # parent from the first parent.
-    tmpSelect = random.randrange(0, tmpLen);
+    tmpSelect = random.randrange(0, tmpLen)
 
     for element in appointmentsByVehicle2[tmpSelect]:
         for index in range(0, tmpLen):
@@ -123,14 +123,14 @@ def crossOver(parent1, parent2):
         appointmentsByVehicle1 = insertAppointment(
                 element,
                 appointmentsByVehicle1
-                );
+                )
 
     print("After: ")
-    print(appointmentsByVehicle1);
+    print(appointmentsByVehicle1)
 
     # Making new vehicles containing the right number of appointments for the
     # offspring.
-    vehicleList = [];
+    vehicleList = []
 
     for index in range(0, tmpLen):
         vehicleList.append(
@@ -138,43 +138,43 @@ def crossOver(parent1, parent2):
                     parent1._vehicles()[index]._idVehicle(),
                     len(appointmentsByVehicle1[index])
                     )
-                );
+                )
 
         # Flattening the 2D list to create the offspring.
-    appointments = [i for subList in appointmentsByVehicle1 for i in subList];
+    appointments = [i for subList in appointmentsByVehicle1 for i in subList]
 
     # Yay! Offspring!
-    return Individual(appointments, vehicleList);
+    return MvrpIndividual(appointments, vehicleList)
 
-ap1 = Appointment(1, 1);
-ap2 = Appointment(2, 2);
-ap3 = Appointment(3, 3);
-ap4 = Appointment(4, 4);
-ap5 = Appointment(5, 5);
-ap6 = Appointment(6, 6);
+ap1 = Appointment(1, 1)
+ap2 = Appointment(2, 2)
+ap3 = Appointment(3, 3)
+ap4 = Appointment(4, 4)
+ap5 = Appointment(5, 5)
+ap6 = Appointment(6, 6)
 
-vehicle1 = Vehicle(1, 2);
-vehicle2 = Vehicle(2, 3);
-vehicle3 = Vehicle(3, 1);
-vehicle4 = Vehicle(4, 1);
-vehicle5 = Vehicle(5, 4);
-vehicle6 = Vehicle(6, 1);
+vehicle1 = Vehicle(1, 2)
+vehicle2 = Vehicle(2, 3)
+vehicle3 = Vehicle(3, 1)
+vehicle4 = Vehicle(4, 1)
+vehicle5 = Vehicle(5, 4)
+vehicle6 = Vehicle(6, 1)
 
-parent1 = Individual(
+parent1 = MvrpIndividual(
         [ap1, ap2, ap3, ap4, ap5, ap6],
-        [vehicle1, vehicle2, vehicle3]);
-parent2 = Individual(
+        [vehicle1, vehicle2, vehicle3])
+parent2 = MvrpIndividual(
         [ap6, ap3, ap5, ap2, ap4, ap1],
-        [vehicle4, vehicle5, vehicle6]);
+        [vehicle4, vehicle5, vehicle6])
 
-result = crossOver(parent1, parent2);
+result = crossOver(parent1, parent2)
 
-print("\n");
-print(parent1._appointments());
-print(parent1._vehicles());
-print("\n");
-print(parent2._appointments());
-print(parent2._vehicles());
-print("\n");
-print(result._appointments());
-print(result._vehicles());
+print("\n")
+print(parent1._routes())
+print(parent1._vehicles())
+print("\n")
+print(parent2._routes())
+print(parent2._vehicles())
+print("\n")
+print(result._routes())
+print(result._vehicles())
--- vrptw.py
+++ vrptw.py
@@ -31,68 +31,68 @@ def main():
     crossover_probability = 0.7
     mutation_probability = 0.7
     ngen = 5
-    
+
     # Assign the custom individual class to the toolbox
     # And set the number of wanted fitnesses 
     toolbox = base.Toolbox()
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,-1.0))
     creator.create("Individual", genome.MvrpIndividual, fitness=creator.FitnessMin)
-    
+
     # Assign the initialisation operator to the toolbox's individual
     # And describe the population initialisation  
     toolbox.register("individual", operators.init, creator.Individual,
-                     size = IND_SIZE, nb_vehicle = num_route)
+            size = IND_SIZE, nb_vehicle = num_route)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    
+
     # Generate a the problem's data set
     # i.e: Generate N "route" of appointement 
     list_appointment = model.generate_route(num_route, 
-                                             num_node_per_route,
-                                             w,
-                                             h,
-                                             depot)
+            num_node_per_route,
+            w,
+            h,
+            depot)
     # Set the routes color  
     color = visualisation.color_group(num_route)
-    
+
     # Set the different genetic oprator inside the toolbox
     toolbox.register("clone", copy.deepcopy)
-    toolbox.register("mate", operators.cxRC)
+    toolbox.register("mate", operators.cxRC, data=list_appointment)
     toolbox.register("mutate", operators.constrainedSwap, data=list_appointment)
     toolbox.register("select", tools.selNSGA2)
     toolbox.register("evaluate", operators.evaluate, data=list_appointment, depot=depot)
-    
+
     # Create the global population
     # And an elite one  
 
     pop = toolbox.population(n=pop_size)
     hof = tools.HallOfFame(elite_size)
-    
+
     # Create a statistic module to display stats at each generation 
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
     stats.register("std", numpy.std)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
-    
-    
+
+
     # The genetic alogorithm in itself 
     algorithms.eaSimple(pop, 
-                        toolbox,
-                        crossover_probability,
-                        mutation_probability,
-                        ngen,
-                        stats=stats, 
-                        halloffame=hof)
+            toolbox,
+            crossover_probability,
+            mutation_probability,
+            ngen,
+            stats=stats, 
+            halloffame=hof)
 
     # Create display of the problem and of the best solution  
     root = visualisation.Tk()
     root.geometry("" + str(w) + "x" + str(h))
     app = visualisation.Example(root, 
-                                list_appointment,
-                                color, 
-                                depot,
-                                visualisation.individual_as_appointment(hof[0], list_appointment))
-    
+            list_appointment,
+            color, 
+            depot,
+            visualisation.individual_as_appointment(hof[0], list_appointment))
+
     # Start the GUI main loop
     root.mainloop()  
 
